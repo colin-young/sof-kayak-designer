d588c4de0a1a94c3bbf63d1ee7f8377a8d0de879
Colin Young <colin.young.todo@gmail.com>
2015-01-22 22:25:15 -0500

    Fixed flex layout.

--- canvas.js
+++ canvas.js
@@ -1,10 +1,5 @@
-var path = new Path();
-path.strokeColor = '#567e37';
-path.strokeWidth = 5;
-
-var firstPoint = new Point(0, 550);
-path.add(firstPoint);
-
-var throughPoint = new Point(75, 400);
-var toPoint = new Point(100, 250);
-path.arcTo(throughPoint, toPoint); 
+var path = new Path.Circle({
+    center: view.center,
+    radius: 30,
+    strokeColor: 'black'
+});
